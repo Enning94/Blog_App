a5db06ae4eb0add2fa342b648ea9e18e3bb551bd
Enning94 <enningfestus@gmail.com>
2023-08-17 10:04:28 +0000

    Fix linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def update_likes_counter_for_post
-        return unless post.present?
-        post.update(likes_counter: post.likes_counter.to_i + 1)
-    end
+  def update_likes_counter_for_post
+    return unless post.present?
+
+    post.update(likes_counter: post.likes_counter.to_i + 1)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    after_create :update_user_posts_counter
+  after_create :update_user_posts_counter
 
-    def update_user_posts_counter
-        return unless author.present?
-  author.update(posts_counter: author.posts_counter.to_i + 1)
-    end
+  def update_user_posts_counter
+    return unless author.present?
 
-    def recent_comments_for_post
-        comments.order(created_at: :desc).limit(5)
-    end
+    author.update(posts_counter: author.posts_counter.to_i + 1)
+  end
+
+  def recent_comments_for_post
+    comments.order(created_at: :desc).limit(5)
+  end
 end
-
