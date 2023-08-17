06177f00e521e562e5b5174d0b18755cf6f6f50f
Enning94 <enningfestus@gmail.com>
2023-08-17 09:50:46 +0000

    Created a method that updates the posts counter for a user
    Created a  method which returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    after_create :update_user_posts_counter
+
+    def update_user_posts_counter
+        return unless author.present?
+  author.update(posts_counter: author.posts_counter.to_i + 1)
+    end
+
+    def recent_comments_for_post
+        comments.order(created_at: :desc).limit(5)
+    end
 end
+
