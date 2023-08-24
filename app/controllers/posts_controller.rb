585ebf3e953cbe1b58e05050fe16522f7b72a31c
Enning94 <enningfestus@gmail.com>
2023-08-24 01:29:39 +0000

    Implemented the design from the sneak peek wireframes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
 
-  def show; end
+  def index
+   @user = User.find_by(id: params[:user_id])
+   
+      @posts = @user.posts.includes(:comments)
+
+  end
+
+  def show;
+    @post = Post.includes(:author, comments: :author).find(params[:id])
+  end
 end
