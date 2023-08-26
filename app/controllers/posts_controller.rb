1d31ac6582eec67d93ce6e31e74fa7105d9f82a1
Enning94 <enningfestus@gmail.com>
2023-08-26 10:48:55 +0000

    create forms to creates a Post on behalf of the current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.includes(:author, comments: :author).find(params[:id])
   end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @user = current_user
+    @post.author = @user
+    if @post.save
+      flash[:success] = 'User posted successfully'
+      redirect_to user_posts_path(@user)
+    else
+      flash.now[:error] = 'Post not saved'
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
