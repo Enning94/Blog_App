463d18560a30fcabd52a7c7cd20aef5e9fa6ae32
Enning94 <enningfestus@gmail.com>
2023-08-29 19:52:28 +0000

    Implement requested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
-    @posts = Post.includes(:author).paginate(page: params[:page], per_page: 3)
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 3)
   end
 
   def show
