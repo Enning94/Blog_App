6cecb6c9948b70bb586c2772f8a40cb3996e4036
WilhelmK109 <kamulungaw@gmail.com>
2023-09-01 12:09:55 +0200

    Update user ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,11 @@ class Ability
     return unless user.present?
 
     can :read, Post
+    can :create, Comment
+    can :read, Comment
+    can :destroy, Comment, author_id: user.id
     can :manage, Post, author_id: user.id
+
     return unless user.admin?
 
     can :manage, :all
